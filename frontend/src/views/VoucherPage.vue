7b0a330bb739350581e8194a0f8dbccf9b5b5d8e
weiyifei <993159835@qq.com>
2025-12-22 20:32:28 +0800

    初始化

---
+++ frontend/src/views/VoucherPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card>
+    <template #header>记账凭证</template>
+    <el-button type="primary">新增凭证</el-button>
+    <el-table :data="[]">
+      <el-table-column prop="code" label="凭证号" />
+      <el-table-column prop="date" label="日期" />
+      <el-table-column prop="debit" label="借方" />
+      <el-table-column prop="credit" label="贷方" />
+    </el-table>
+    <el-divider />
+    <el-button>借贷平衡校验</el-button>
+  </el-card>
+</template>
+
+<script setup>
+</script>
+
+
+
